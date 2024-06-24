fed303783ec641fb3f805a9e8228ff627e957602
bskrobich <bartskrobich@gmail.com>
2024-06-24 20:58:23 +0200

    Implemented AddPersonClient method in PersonService(Client)

---
+++ Services/ClientServices/PersonService.cs
@@ -0,0 +1,42 @@
+using RevenueRecognitionSystem.Contexts;
+using RevenueRecognitionSystem.Models;
+using RevenueRecognitionSystem.RequestModels;
+
+namespace RevenueRecognitionSystem.Services.ClientServices;
+
+public interface IPersonService
+{
+    Task AddPersonClient(AddPersonClientRequestModel request);
+    Task UpdatePersonClient(UpdatePersonClientRequestModel request, string PESEL);
+    Task DeletePersonClient();
+
+}
+
+public class PersonService(DatabaseContext dbContext) : IPersonService
+{
+    public async Task AddPersonClient(AddPersonClientRequestModel request)
+    {
+        var personClient = new Person
+        {
+            PESEL = request.PESEL,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Address = request.Address,
+            Email = request.Email,
+            PhoneNumber = request.PhoneNumber
+        };
+
+        await dbContext.People.AddAsync(personClient);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public Task UpdatePersonClient(UpdatePersonClientRequestModel request, string PESEL)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeletePersonClient()
+    {
+        throw new NotImplementedException();
+    }
+}
